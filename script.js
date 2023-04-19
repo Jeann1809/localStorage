d8712ec4554745b0ad38aec3a33534b7f5635e9f
Jeann1809 <carlojeanalmario@gmail.com>
2023-04-19 18:27:14 -0500

    localStorage

---
+++ script.js
@@ -0,0 +1,9 @@
+// localStorage.setItem("llave", `{value:"valor", aval: "valor2"}`)
+let n = `[1,2,3,4,5]`;
+let persona = { nombre: "jean", apellido: "almario", edad: "18" };
+console.log(JSON.stringify(persona));
+console.log(n.toString());
+console.log(n);
+console.log(persona);
+
+                
